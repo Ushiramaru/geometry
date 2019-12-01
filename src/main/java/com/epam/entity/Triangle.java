d9293c6493b91dbd690c699004399c60a2226ab2
Асус <https://github.com/Ushiramaru/OOP_1.git>
2019-12-01 18:55:50 +0300

    Adjusted according to requirements

--- src/main/java/com/epam/entity/Triangle.java
+++ src/main/java/com/epam/entity/Triangle.java
@@ -2,55 +2,59 @@ package com.epam.entity;
 
 public class Triangle {
 
-    private Point point1;
-    private Point point2;
-    private Point point3;
+    private Point pointFirst;
+    private Point pointSecond;
+    private Point pointThird;
 
     public Triangle(Point[] points) {
-        point1 = points[0];
-        point2 = points[1];
-        point3 = points[2];
+        pointFirst = points[0];
+        pointSecond = points[1];
+        pointThird = points[2];
     }
 
-    public Triangle(Point point1, Point point2, Point point3) {
-        this.point1 = point1;
-        this.point2 = point2;
-        this.point3 = point3;
+    public Triangle(Point pointFirst, Point pointSecond, Point pointThird) {
+        this.pointFirst = pointFirst;
+        this.pointSecond = pointSecond;
+        this.pointThird = pointThird;
     }
 
-    public Point getPoint1() {
-        return point1;
+    public Point getPointFirst() {
+        return pointFirst;
     }
 
-    public Point getPoint2() {
-        return point2;
+    public Point getPointSecond() {
+        return pointSecond;
     }
 
-    public Point getPoint3() {
-        return point3;
+    public Point getPointThird() {
+        return pointThird;
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Triangle triangle = (Triangle) o;
-        return point1.equals(triangle.point1) &&
-                point2.equals(triangle.point2) &&
-                point3.equals(triangle.point3);
+        return pointFirst.equals(triangle.pointFirst) &&
+                pointSecond.equals(triangle.pointSecond) &&
+                pointThird.equals(triangle.pointThird);
     }
 
     @Override
     public int hashCode() {
-        return 31 * (31 * (31 + point1.hashCode()) + point2.hashCode()) + point3.hashCode();
+        return 31 * (31 * (31 + pointFirst.hashCode()) + pointSecond.hashCode()) + pointThird.hashCode();
     }
 
     @Override
     public String toString() {
         return "Triangle{" +
-                "point1=" + point1 +
-                ", point2=" + point2 +
-                ", point3=" + point3 +
+                "pointFirst=" + pointFirst +
+                ", pointSecond=" + pointSecond +
+                ", pointThird=" + pointThird +
                 '}';
     }
 
